4de97e907b6c0df48c75df64efa76870332c8b57
Seungyeon0321 <71824405+Seungyeon0321@users.noreply.github.com>
2024-04-08 16:31:55 -0700

    fixed middleware

--- back/app.js
+++ back/app.js
@@ -1,21 +1,3 @@
-//노드가 서버를 해주는 게 아니라 이 http가 해주는 것이다
-//express는 이러한 노드의 작업을 깔끔하게 해주는 framework라고 생각하면 된다
-//서버를 돌릴려면 이렇게 http가 가능해야 하는데 express도 가능하다, 즉 노드에서 제공하는 http 모듈이 서버라고 생각하면 된다
-/////////////node js////////////////////
-// const http = require("http");
-// const server = http.createServer((req, res) => {
-//   console.log(req.rul, res.method);
-//   //이런식으로 html 태그도 넣을 수 있다
-//   res.write("<h1>hello node1</h1>");
-//   res.write("<h2>hello node2</h2>");
-//   res.write("hello node3");
-//   res.write("hello node4");
-//   res.write("hello node5");
-//   res.end("hello node");
-//   //res.end는 두번 사용하면 안 된다
-// });
-
-//////////////express////////////////////
 const express = require("express");
 const cors = require("cors");
 const postRouter = require("./routes/post");
@@ -37,9 +19,13 @@ dotenv.config();
 db.sequelize
   .sync()
   .then(() => {
-    console.log("db 연결 성공");
+    console.log("Connected DB successfully!!");
   })
   .catch(console.error);
+
+passportConfig();
+
+app.use(cookieParser(process.env.COOKIE_SECRET));
 
 app.use(
   session({
@@ -48,11 +34,10 @@ app.use(
     secret: process.env.COOKIE_SECRET,
   })
 );
-app.use(cookieParser(process.env.COOKIE_SECRET));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
-passportConfig();
 app.use(morgan("dev"));
 
 app.use(express.json());
