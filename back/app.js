df7184a9a212e2b0ad800b6764af23700a808ed1
Seungyeon0321 <71824405+Seungyeon0321@users.noreply.github.com>
2024-04-08 15:06:10 -0700

    corrected some filed

--- back/app.js
+++ back/app.js
@@ -41,23 +41,6 @@ db.sequelize
   })
   .catch(console.error);
 
-passportConfig();
-app.use(morgan("dev"));
-//이녀석들이 front에서 넘어오는 data를 해석해서 routes user의 req.body로 데이터를 넣어준다, 최대한 위에 있어야 함 (해당 부분이 middle ware인데 아직 잘 모르겠음)
-app.use(express.json()); //front에서 json파일이 넘어오면 그 파일을 req.body에 넣어주게 되고
-app.use(
-  cors({
-    origin: true, //origin: true로 설정해두면 * 대신 보낸 곳의 주소가 자동으로 들어가 편리함
-    credentials: true,
-  })
-);
-//이 static을 이용하면 사진 미리보기가 가능해진다
-//path.join을 사용하는 이유는 운영체제에 따라 파일 이름이 바뀌기 때문에 단순히 __dirname + "uploads" 이렇게 못 지은다
-//그리고 앞에 오는 '/' localhost:3065가 된다
-//이렇게 하면 좋은게 프론트에서는 /을 이용해서 해당 사진에 접근
-//할 수 있는 점이다. 그렇기 때문에 프론트에서는 서버쪽 폴더 구조를 잘 모른다 (보안에 좀 더 유용한 이점이 있다)
-app.use("/", express.static(path.join(__dirname, "uploads")));
-app.use(express.urlencoded({ extended: true })); //form submit했을 때 url 인코딩 방식으로 넘어오게 됨
 app.use(
   session({
     saveUninitialized: false,
@@ -69,18 +52,23 @@ app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(passport.initialize());
 app.use(passport.session());
 
-//여기서 get이 url이고 /가 method이다
+passportConfig();
+app.use(morgan("dev"));
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+
+app.use("/", express.static(path.join(__dirname, "uploads")));
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("<h1>hello express</h1>");
 });
-
-//이런식으로 if문을 안쓰고 만들어줄 수 있다
-
-//post랑 delete요청은 이렇게 api주소로는 할 수 없다, 이건 get이기 때문에
-
-//이런식으로 다른 파일안에 있는 라우터를 가지고 옴으로써 라우터를 분리시킬 수 있음, 중복되는 녀석의 주소를 이렇게 처음 매개변수로 대입
-
-//여기 post and user가 prefix로 붙고 그 다음 주소가 router 안에 있는 라우터의 주소가 오게 됨 예를 들어서 postRouter안에 post라는 router가 있게 되면 실제 주소는 post/post가 됨
 
 app.use("/post", postRouter);
 app.use("/posts", postsRouter);
@@ -90,9 +78,3 @@ app.use("/hashtag", hashtagRouter);
 app.listen(3065, () => {
   console.log("서버 실행 중");
 });
-
-// 서버의 기본 프론트에서 요청을 해주면 back에서 응답을 해준다가 기본 원리이다
-// 응답을 안 보내면 특정 시간(30초 정도)후에 브라우저가 자동으로 응답 실패로 처리해버립니다
-//여러개의 파일이 필요할때는 한번의 요청에 대비해, 여러개의 파일을 묶어서 응답을 보내거나 아니면 요청을 여러번 보내서 그 요청에 맞게 데이터를 묶어서 보내던가 해야한다
-
-//라우터가 다양한 이유가 프론트에서 여러가지 다양한 상황에서 데이터를 요구하기 때문에 거기에 맞춰서 데이터를 보내줘야 하기 때문에 여러가지 라우터를 필요로 한다 (하나의 요청은 하나의 응답만 할 수 있기 때문에!)
