0b074cc6d2c46e45a0907a15195b45e890ee6912
Seungyeon0321 <71824405+Seungyeon0321@users.noreply.github.com>
2024-05-16 08:42:57 -0700

    fixed error

--- front/sagas/index.js
+++ front/sagas/index.js
@@ -4,7 +4,7 @@ import postSaga from "./post";
 import userSaga from "./user";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:80";
+axios.defaults.baseURL = "http://35.183.35.12";
 axios.defaults.withCredentials = true;
 
 export default function* rootSaga() {
