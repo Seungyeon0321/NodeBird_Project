bb7b5aeeaf5984d8f5a2db297f4175d31c2fe48a
Seungyeon0321 <71824405+Seungyeon0321@users.noreply.github.com>
2024-07-06 00:07:33 -0700

    feat: Added cookie

--- back/app.js
+++ back/app.js
@@ -53,6 +53,14 @@ app.use(
     saveUninitialized: false,
     resave: false,
     secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+      domain:
+        process.env.NODE_ENV === "production"
+          ? ".portfolio-simon.com"
+          : "localhost",
+    },
   })
 );
 app.use(passport.initialize());
