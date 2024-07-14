d7085c33afe0e6bd1f0c84fba1d19971246d4be5
Seungyeon0321 <71824405+Seungyeon0321@users.noreply.github.com>
2024-07-13 21:25:02 -0700

    feat: Correct version of aws-sdk

--- front/config/config.js
+++ front/config/config.js
@@ -1,1 +1,2 @@
 export const backURL = "http://api.portfolio-simon.com";
+// export const backURL = "http://localhost:80";
--- lambda/index.js
+++ lambda/index.js
@@ -1,14 +1,18 @@
-const AWS = require("aws-sdk");
+const {
+  S3Client,
+  GetObjectCommand,
+  PutObjectCommand,
+} = require("@aws-sdk/client-s3");
 const sharp = require("sharp");
 
-const s3 = new AWS.S3();
+const s3 = new S3Client();
 
 exports.handler = async (event, context, callback) => {
-  const Bucket = event.Records[0].s3.bucket.name; //portfolio-simon-nodebird
-  const Key = decodeURIComponent(event.Records[0].s3.object.key); //f
+  const Bucket = event.Records[0].s3.bucket.name;
+  const Key = decodeURIComponent(event.Records[0].s3.object.key);
   console.log(Bucket, Key);
   const filename = Key.split("/")[Key.split("/").length - 1];
-  const ext = Key.split(".")[Key.split(".").length - 1].toLowerCase(); //대문자 jpg를 소문자로 변경
+  const ext = Key.split(".")[Key.split(".").length - 1].toLowerCase();
   const requiredFormat = ext === "jpg" ? "jpeg" : ext;
   console.log(
     "filename",
@@ -19,16 +23,20 @@ exports.handler = async (event, context, callback) => {
     requiredFormat
   );
   try {
-    const s3Object = await s3.getObject({ Bucket, Key }).promise();
+    const getObjectCommand = new GetObjectCommand({ Bucket, Key });
+    const s3Object = await s3.send(getObjectCommand);
     const resizedImage = await sharp(s3Object.Body)
       .resize(400, 400, { fit: "inside" })
       .toFormat(requiredFormat)
       .toBuffer();
 
-    await s3
-      .putObject({ Bucket, Key: `thumb/${filename}`, Body: resizedImage })
-      .promise();
-
+    const putObjectCommand = new PutObjectCommand({
+      Bucket,
+      Key: `thumb/${filename}`,
+      Body: resizedImage,
+    });
+    await s3.send(putObjectCommand);
+    console.log("put", resizedImage.length);
     return callback(null, `thumb/${filename}`);
   } catch (error) {
     console.error(error);
