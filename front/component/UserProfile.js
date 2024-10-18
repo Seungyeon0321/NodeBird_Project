252fc247f08812a601fa56bc46950dede7cf668e
Seungyeon0321 <71824405+Seungyeon0321@users.noreply.github.com>
2024-10-17 20:18:13 -0700

    feat: fixed Profile Component

--- front/component/UserProfile.js
+++ front/component/UserProfile.js
@@ -14,51 +14,53 @@ const UserProfile = () => {
   }, []);
 
   return (
-    <Card
-      style={{ width: 300 }}
-      actions={[
-        <div key="twit">
-          <Link href={`/user/${me.id}`}>
-            Twit
-            <br />
-            {me.Posts.length}
-          </Link>
-        </div>,
-        <div key="followings">
-          <Link href={`/profile`}>
-            Followings
-            <br />
-            {me.Followings.length}
-          </Link>
-        </div>,
-        <div key="followers">
-          <Link href={`/profile`}>
-            Followers
-            <br />
-            {me.Followers.length}
-          </Link>
-        </div>,
-      ]}
-    >
-      <Meta
-        avatar={
-          <Link href={`/user/${me.id}`} prefetch={false}>
-            <Avatar
-              shape="square"
-              size={64}
-              icon={<UserOutlined />}
-              title={me.nickname}
-            ></Avatar>
-          </Link>
-        }
-        title={me.nickname}
-        description={
-          <Button onClick={logoutHandler} loading={isLoggingOut}>
-            Log Out
-          </Button>
-        }
-      />
-    </Card>
+    me && (
+      <Card
+        style={{ width: 300 }}
+        actions={[
+          <div key="twit">
+            <Link href={`/user/${me.id}`}>
+              Twit
+              <br />
+              {me.Posts.length}
+            </Link>
+          </div>,
+          <div key="followings">
+            <Link href={`/profile`}>
+              Followings
+              <br />
+              {me.Followings.length}
+            </Link>
+          </div>,
+          <div key="followers">
+            <Link href={`/profile`}>
+              Followers
+              <br />
+              {me.Followers.length}
+            </Link>
+          </div>,
+        ]}
+      >
+        <Meta
+          avatar={
+            <Link href={`/user/${me.id}`} prefetch={false}>
+              <Avatar
+                shape="square"
+                size={64}
+                icon={<UserOutlined />}
+                title={me.nickname}
+              ></Avatar>
+            </Link>
+          }
+          title={me.nickname}
+          description={
+            <Button onClick={logoutHandler} loading={isLoggingOut}>
+              Log Out
+            </Button>
+          }
+        />
+      </Card>
+    )
   );
 };
 
