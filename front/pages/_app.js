52526d6eb893c3e87c0869a55d378d013631e47e
Seungyeon0321 <71824405+Seungyeon0321@users.noreply.github.com>
2024-10-19 00:04:09 -0700

    add: Added meta data

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -9,6 +9,27 @@ const NodeBird = ({ Component }) => {
       <Head>
         <meta charSet="utf-8" />
         <title>Node Bird</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="A social media platform for bird enthusiasts"
+        />
+        <meta property="og:title" content="Node Bird" />
+        <meta
+          property="og:description"
+          content="Connect with other bird lovers and share your bird watching experiences"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://portfolio-simon.com" />
+        <meta
+          property="og:image"
+          content="https://portfolio-simon-nodebird.s3.ca-central-1.amazonaws.com/nodebird_logo.jpg"
+        />
+        <meta property="og:image:alt" content="NodeBird Logo" />
+        <meta property="og:site_name" content="Node Bird" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@nodebird" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component />
     </>
