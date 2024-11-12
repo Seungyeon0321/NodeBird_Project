8d8047c5391937691d6ca4a5589741050cadc39a
Seungyeon0321 <71824405+Seungyeon0321@users.noreply.github.com>
2024-11-11 18:07:31 -0800

    feat:fixed Lambda code

--- lambda/index.js
+++ lambda/index.js
@@ -9,8 +9,8 @@ const s3 = new S3Client();
 
 exports.handler = async (event, context, callback) => {
   const Bucket = event.Records[0].s3.bucket.name;
-  const Key = decodeURIComponent(event.Records[0].s3.object.key);
-
+  const Key = decodeURIComponent(event.Records[0].s3.object.key); //Korean
+  console.log(Key);
   const filename = Key.split("/")[Key.split("/").length - 1];
   const ext = Key.split(".")[Key.split(".").length - 1].toLowerCase();
   const requiredFormat = ext === "jpg" ? "jpeg" : ext;
@@ -28,11 +28,17 @@ exports.handler = async (event, context, callback) => {
       Key: `thumb/${filename}`,
       Body: resizedImage,
     });
+
     await s3.send(putObjectCommand);
     console.log("put", resizedImage.length);
+    //we send the invoker the path of the resized image
     return callback(null, `thumb/${filename}`);
   } catch (error) {
     console.error(error);
     return callback(error);
   }
 };
+
+function getThumbnailPath(key) {
+  return key.split("/")[key.split("/").length - 1];
+}
